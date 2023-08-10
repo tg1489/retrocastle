3ad63828d01e8d6bcb1be0e0d023a201ebaafa60
Tony <tonyguarino1489@gmail.com>
2023-08-10 16:51:09 -0400

    about page mostly done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,8 +6,13 @@ export default function Home() {
       <div className='row'>
         <p>Hi! Welcome to RetroCastle!</p>
       </div>
-      <div className='row'></div>
-      <div className='row'></div>
+      <div className='row'>
+        <p>Text on what the website is about</p>
+      </div>
+      <div className='row'>
+        Talk about what products I make the most and how I offer personal custom
+        ones. (Link to Gallery too)
+      </div>
     </div>
   );
 }
