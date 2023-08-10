ab255687d0de1be6e38dafdd6bb78d4da64e91f7
Tony <tonyguarino1489@gmail.com>
2023-08-10 18:29:46 -0400

    I think I am finished with requests

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,18 +1,24 @@
 import React, { useRef, useState } from 'react';
 import emailjs from 'emailjs-com';
+import '../Form.css';
 
 export const Form = () => {
+  // Ref to the form element
   const form = useRef();
+
+  // State variables to track name, email, and warning visibility
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [nameWarningVisible, setNameWarningVisible] = useState(false);
   const [emailWarningVisible, setEmailWarningVisible] = useState(false);
 
+  // Email validation function
   const validateEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
 
+  // Event handlers for name and email inputs
   const handleNameChange = (e) => {
     setName(e.target.value);
     setNameWarningVisible(false); // Hide the name warning when the name input changes
@@ -23,6 +29,7 @@ export const Form = () => {
     setEmailWarningVisible(false); // Hide the email warning when the email input changes
   };
 
+  // Event handlers for name and email blur (focus out)
   const handleNameBlur = () => {
     if (name.trim() === '') {
       setNameWarningVisible(true); // Show the name warning if the name field is empty on blur
@@ -35,9 +42,11 @@ export const Form = () => {
     }
   };
 
+  // Event handler for form submission
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
+    // Check name and email for validation
     if (name.trim() === '') {
       setNameWarningVisible(true); // Show the name warning if the name field is empty on form submit
     }
@@ -46,8 +55,8 @@ export const Form = () => {
       setEmailWarningVisible(true); // Show the email warning if the email field is empty or does not contain a valid email on form submit
     }
 
+    // Send the email using emailjs if both name and email are valid
     if (name.trim() && email.trim() && validateEmail(email)) {
-      // If both name and email have valid values, send the email using emailjs
       emailjs
         .sendForm(
           'service_2z4jx6e',
@@ -67,39 +76,43 @@ export const Form = () => {
   };
 
   return (
-    <form className='form-contact' ref={form} onSubmit={handleFormSubmit}>
-      <p className='form-contact-text'>Your Name</p>
-      <input
-        required
-        type='text'
-        name='user_name'
-        value={name}
-        onChange={handleNameChange}
-        onBlur={handleNameBlur}
-      />
-      {nameWarningVisible && (
-        <span className='warning'>⚠️ Name is required</span>
-      )}
-      <p className='form-contact-text'>Your Email</p>
-      <input
-        required
-        type='email'
-        name='user_email'
-        value={email}
-        onChange={handleEmailChange}
-        onBlur={handleEmailBlur}
-      />
-      {emailWarningVisible && (
-        <span className='warning'>⚠️ Please enter a valid email</span>
-      )}
-      <p className='form-contact-text'>Message</p>
-      <textarea name='message' />
-      <input
-        required
-        className='form-contact-button'
-        type='submit'
-        value='Send'
-      />
-    </form>
+    <>
+      {/* Form */}
+      <form className='form-contact' ref={form} onSubmit={handleFormSubmit}>
+        <p className='form-contact-text'>Your Name</p>
+        <input
+          required
+          type='text'
+          name='user_name'
+          value={name}
+          onChange={handleNameChange}
+          onBlur={handleNameBlur}
+        />
+        {nameWarningVisible && (
+          <span className='warning'>⚠️ Name is required</span>
+        )}
+        <p className='form-contact-text'>Your Email</p>
+        <input
+          required
+          type='email'
+          name='user_email'
+          value={email}
+          onChange={handleEmailChange}
+          onBlur={handleEmailBlur}
+        />
+        {emailWarningVisible && (
+          <span className='warning'>⚠️ Please enter a valid email</span>
+        )}
+        <p className='form-contact-text'>Message</p>
+        <textarea name='message' />
+        <input
+          required
+          className='form-contact-button'
+          type='submit'
+          value='Send'
+        />
+      </form>
+      <br />
+    </>
   );
 };
--- client/src/pages/Requests.jsx
+++ client/src/pages/Requests.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import email from '../assets/email.png';
-import call from '../assets/call.png';
 import { Form } from '../components/Form';
+import '../Requests.css';
 
 export default function Requests() {
   return (
-    <div>
-      <h2 className='contact-title'>Let's get in touch</h2>
+    <div className='container'>
+      <h2 className='requests-title'>Have a request?</h2>
+      <p className='requests-text'>
+        If the existing options do not align with your preferences, I invite you
+        to explore the possibility of a custom design tailored exclusively to
+        your vision. Your input is of utmost importance, and I am open to
+        crafting a design that resonates with your individual style. Feel free
+        to communicate your preferences, ideas, or even provide reference
+        images, and I will diligently bring your artistic aspirations to life.
+        Your satisfaction is my priority, and I am committed to delivering a
+        personalized piece that exceeds your expectations.
+      </p>
       <img src={email} alt='email logo' className='contact-image' />
-      <h5 className='contact-body'>tonyguarino1489@gmail.com</h5>
+      <h5 className='requests-email'>retrocastlenj@gmail.com</h5>
       <br />
-      <img src={call} alt='phone logo' className='contact-image' />
-      <h5 className='contact-body'>732-850-6458</h5>
       <div className='container'>
         <Form />
       </div>
