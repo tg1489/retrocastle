4a0182244489e1e08c89d3cbe1ad88dce4cdee28
Tony <tonyguarino1489@gmail.com>
2023-08-14 20:44:00 -0400

    Set up etsy page for now

--- client/src/pages/Etsy.jsx
+++ client/src/pages/Etsy.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import '../Etsy.css';
+import logo from '../assets/logo-old.png';
 
 export default function Etsy() {
   return (
-    <div>Etsy</div>
-  )
+    <div className='container border-container-etsy'>
+      <div className='row'>
+        <div className='col'>
+          <img src={logo} className='logo-old' />
+          <p className='etsy-paragraph'>
+            Explore a world of unique and handmade products that makes a perfect
+            gift for any occasion. Each item is crafted with passion and
+            attention to detail.
+          </p>
+          <a
+            href='https://www.etsy.com/people/wyq40wlevkr85zrv?ref=hdr_user_menu-profile'
+            className='btn btn-primary'
+            target='_blank'
+          >
+            Shop Now
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
