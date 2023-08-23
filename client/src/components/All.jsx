e31029f6f1c4ca7393be30cda429118a85fb9ffa
Tony <tonyguarino1489@gmail.com>
2023-08-23 19:00:34 -0400

    Gallery (All) update

--- client/src/components/All.jsx
+++ client/src/components/All.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import all from '../utils/all';
+import '../All.css';
 
 export default function All() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 600);
+    };
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
+
   return (
     <div className='container'>
       <div className='row'>
         {all.map((image, index) => (
-          <img key={index} src={image.src} className='col s3 gallery-image' />
+          <img
+            key={index}
+            src={image.src}
+            className={isMobile ? 'mobile-all col' : 'col gallery-image'}
+          />
         ))}
       </div>
     </div>
   );
 }
+
+//  <div className={isMobile ? 'col s12' : 'col s3'}>
+//           {all.map((image, index) => (
+//             <img
+//               key={index}
+//               src={image.src}
+//               className={isMobile ? 'mobile-all' : 'gallery-image'}
+//             />
+//           ))}
+//         </div>
--- client/src/utils/all.jsx
+++ client/src/utils/all.jsx
@@ -1,16 +1,29 @@
+import cube1 from '../assets/cube1.jpg';
+import img1 from '../assets/img1.jpg';
+import img2 from '../assets/img2.jpg';
+import img3 from '../assets/img3.png';
+import img4 from '../assets/img4.jpg';
+import img5 from '../assets/img5.jpg';
+import img6 from '../assets/img6.jpg';
+import img7 from '../assets/img7.jpg';
+import img8 from '../assets/img8.jpg';
+import img9 from '../assets/img9.jpg';
+import img10 from '../assets/img10.jpg';
+import img11 from '../assets/img11.jpg';
+
 const all = [
-  {
-    src: 'https://www.pockettactics.com/wp-content/sites/pockettactics/2021/07/botw-link-2.jpg',
-  },
-  {
-    src: 'https://www.pockettactics.com/wp-content/sites/pockettactics/2021/07/botw-link-2.jpg',
-  },
-  {
-    src: 'https://www.pockettactics.com/wp-content/sites/pockettactics/2021/07/botw-link-2.jpg',
-  },
-  {
-    src: 'https://www.pockettactics.com/wp-content/sites/pockettactics/2021/07/botw-link-2.jpg',
-  },
+  { src: img1 },
+  { src: img2 },
+  { src: img3 },
+  { src: img4 },
+  { src: img5 },
+  { src: img6 },
+  { src: img7 },
+  { src: img8 },
+  { src: img9 },
+  { src: img10 },
+  { src: img11 },
+  { src: cube1 },
 ];
 
 export default all;
