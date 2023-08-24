581971707fb846040d547b14289a670a68f6d6d0
Tony <tonyguarino1489@gmail.com>
2023-08-23 21:07:34 -0400

    Made some changes

--- client/src/components/All.jsx
+++ client/src/components/All.jsx
@@ -4,6 +4,8 @@ import '../All.css';
 
 export default function All() {
   const [isMobile, setIsMobile] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
 
   useEffect(() => {
     const handleResize = () => {
@@ -18,20 +20,25 @@ export default function All() {
     };
   });
 
+  const handleClick = (image) => {
+    console.log(image);
+    setModalVisible(true);
+    setSelectedImage(image);
+
+    
+  };
+
   return (
     <div className='container'>
       <div className='row'>
         <div className='col'>
           {all.map((image, index) => (
-            <a
-              key={index}
-              href={image.href}
-              className='anchor-image no-red-bar'
-            >
+            <a key={index} href='#' className='anchor-image no-red-bar'>
               <img
                 key={index}
                 src={image.src}
                 className={isMobile ? 'mobile-all' : 'col s3 gallery-image'}
+                onClick={() => handleClick(image.href)}
               />
             </a>
           ))}
