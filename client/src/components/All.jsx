39f4593cfd23aafe2554842758eb0e001f5e3c54
Tony <tonyguarino1489@gmail.com>
2023-08-23 20:43:27 -0400

    removed useEffect

--- client/src/components/All.jsx
+++ client/src/components/All.jsx
@@ -6,31 +6,6 @@ export default function All() {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Get the modal
-    const modal = document.getElementById('myModal');
-
-    // Get the button that opens the modal
-    const btn = document.getElementById('myBtn');
-
-    // Get the <span> element that closes the modal
-    const span = document.getElementsByClassName('close')[0];
-
-    // When the user clicks on the button, open the modal
-    btn.onclick = function () {
-      modal.style.display = 'block';
-    };
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-      if (event.target == modal) {
-        modal.style.display = 'none';
-      }
-    };
-
-    return () => {};
-  });
-
-  useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 600);
     };
