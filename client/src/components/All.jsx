7fa7c4f9ac7a88b610dca68ad5b9a98274e69e17
Tony <tonyguarino1489@gmail.com>
2023-08-23 21:21:26 -0400

    Corrupt modal appears

--- client/src/components/All.jsx
+++ client/src/components/All.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import all from '../utils/all';
+import M from 'materialize-css'; // Import Materialize CSS library
 import '../All.css';
 
 export default function All() {
@@ -15,6 +16,9 @@ export default function All() {
 
     window.addEventListener('resize', handleResize);
 
+    // Initialize the Materialize modal
+    M.Modal.init(document.querySelectorAll('.modal'));
+
     return () => {
       window.removeEventListener('resize', handleResize);
     };
@@ -24,8 +28,6 @@ export default function All() {
     console.log(image);
     setModalVisible(true);
     setSelectedImage(image);
-
-    
   };
 
   return (
@@ -33,17 +35,38 @@ export default function All() {
       <div className='row'>
         <div className='col'>
           {all.map((image, index) => (
-            <a key={index} href='#' className='anchor-image no-red-bar'>
+            <a
+              key={index}
+              href='#'
+              className='anchor-image no-red-bar modal-trigger'
+              data-target='modal1'
+              onClick={() => handleClick(image.href)}
+            >
               <img
                 key={index}
                 src={image.src}
                 className={isMobile ? 'mobile-all' : 'col s3 gallery-image'}
-                onClick={() => handleClick(image.href)}
               />
             </a>
           ))}
         </div>
       </div>
+      {modalVisible && (
+        <div id='modal1' className='modal'>
+          <div className='modal-content'>
+            <h2>Modal</h2>
+            <img src={selectedImage} alt='full-image' />
+          </div>
+          <div className='modal-footer'>
+            <button
+              className='modal-close waves-effect waves-green btn-flat'
+              onClick={() => setModalVisible(false)}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
