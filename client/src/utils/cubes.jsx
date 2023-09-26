24739f4d0a40e6ff22392194148c05db7ac973d3
Tony <tonyguarino1489@gmail.com>
2023-09-26 11:39:04 -0400

    Updated cube src's

--- client/src/utils/cubes.jsx
+++ client/src/utils/cubes.jsx
@@ -1,8 +1,20 @@
 import cube1 from '../assets/cube1.jpg';
+import cube2 from '../assets/cube2.png';
+import cube3 from '../assets/cube3.png';
+import cube4 from '../assets/cube4.png';
 
 const cubes = [
   {
     src: cube1,
+  },
+  {
+    src: cube2,
+  },
+  {
+    src: cube3,
+  },
+  {
+    src: cube4,
   },
 ];
 
